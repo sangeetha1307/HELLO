23b416721b6e47f2e77fc3d3408c4e539644c952
sangeetha1307 <60722748+sangeetha1307@users.noreply.github.com>
2020-03-05 09:28:50 +0530

    Create helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+     
+int main()
+{
+  cout << " Hello world ! Welcome to docker.\n This is a simple c++ helloworld application " << endl;
+  return 0;
+}
